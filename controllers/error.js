719172c25fa8584a844f2f1d1616ad95baa6fca2
paynej5504 <54283576+paynej5504@users.noreply.github.com>
2021-03-09 11:40:59 -0500

    Delete error.js

--- controllers/error.js
+++
@@ -1,8 +0,0 @@
-//creates get404 function to render 404
-/*exports.get404 = (req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-}*/
-exports.get404 = (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path:'/404' });
-  };
-  
