4090c30c45d89169650212a51a2edfa2f147c23d
paynej5504 <54283576+paynej5504@users.noreply.github.com>
2021-03-26 12:24:37 -0400

    Update shop.js

--- controllers/shop.js
+++ controllers/shop.js
@@ -35,7 +35,7 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   //find all the products
-  Product.fetchAll()
+  Product.find()
   .then(products => {
     res.render('shop/index', {
       prods: products,
